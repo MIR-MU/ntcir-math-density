8a425008e228c1ee2fae704979ea97e49c0da10f
Vít Novotný <witiko@mail.muni.cz>
2018-06-20 02:09:46 +0200

    Fix documentation of estimator::get_estimates

--- ntcir_math_density/estimator.py
+++ ntcir_math_density/estimator.py
@@ -208,7 +208,7 @@ def get_estimates(estimators_tuple, positions, num_workers=1):
 
     Returns
     -------
-    six-tuple of (sequence of float)
+    five-tuple of (sequence of float)
         Estimates of P(relevant), p(position), p(position | relevant), P(position, relevant), and
         P(relevant | position) in the form of histograms.
     """
